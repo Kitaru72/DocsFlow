23e8f7b4afaa672e30c27f7b1f59c7c987b0f9c0
Kitaru72 <ivan.kolesnichenko21@gmail.com>
2026-05-23 01:13:07 +0500

    Добавить обработку ошибок профиля

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from app.profiles import create_student_profile
 def main():
     print("DocsFlow запущен.")
 
-    student = create_student_profile(
-        "Колесниченко Иван Александрович   ",
-        "3403  ",
-        "         НВГУ",
-        " ИСиТ   ",
-    )
-    print(student)
+    try:
+        student = create_student_profile(
+            "Колесниченко Иван Александрович   ",
+            "3403  ",
+            "НВГУ",
+            "ИСиТ",
+        )
+        print(student)
+    except ValueError as error:
+        print(f"Ошибка: {error}")
 
 
 if __name__ == "__main__":
