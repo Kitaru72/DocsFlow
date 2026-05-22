22965e9c3d0d185da2dd0b87027f7c8a329f9bd7
Kitaru72 <ivan.kolesnichenko21@gmail.com>
2026-05-23 00:58:34 +0500

    Add student profile validation

--- app/profiles.py
+++ app/profiles.py
@@ -1,4 +1,21 @@
 def create_student_profile(full_name, group, university, faculty):
+    full_name = full_name.strip()
+    group = group.strip()
+    university = university.strip()
+    faculty = faculty.strip()
+
+    if full_name == "":
+        raise ValueError("ФИО не может быть пустым")
+
+    if group == "":
+        raise ValueError("Группа не может быть пустой")
+
+    if university == "":
+        raise ValueError("Университет не может быть пустой")
+
+    if faculty == "":
+        raise ValueError("Факультет не может быть пустой")
+
     student = {
         "full_name": full_name,
         "group": group,
