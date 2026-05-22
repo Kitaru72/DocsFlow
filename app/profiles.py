3ecfb426db17d4044d2ccc0f6b2d3aba63949ca3
Kitaru72 <ivan.kolesnichenko21@gmail.com>
2026-05-22 23:42:19 +0500

    Add student profile cration

---
+++ app/profiles.py
@@ -0,0 +1,9 @@
+def create_student_profile(full_name, group, university, faculty):
+    student = {
+        "full_name": full_name,
+        "group": group,
+        "university": university,
+        "faculty": faculty,
+    }
+
+    return student
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from app.profiles import create_student_profile
+
+
 def main():
     print("DocsFlow запущен.")
+
+    student = create_student_profile(
+        "Колесниченко Иван Александрович",
+        "3403",
+        "НВГУ",
+        "ИСиТ",
+    )
+    print(student)
+
 
 if __name__ == "__main__":
     main()
